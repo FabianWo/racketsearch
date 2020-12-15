b11ef4e87a10d4f667d64f7f42de11d9a15ac7a1
Fabian Wollmer <Fabian.wollmer@gmail.com>
2020-12-15 18:52:22 +0100

    routesv3

--- helpers/dynamicRoutes.js
+++ helpers/dynamicRoutes.js
@@ -3,6 +3,8 @@
 // const path = require('path');
 const normalizePath = require('normalize-path');
 
+console.log(__dirname.search('/.com/g'));
+
 if ( __dirname.search('/.com/g') !== -1 ) {
   exports.dynamicURL = normalizePath(__dirname.split('.com')[1].split('helpers')[0]);
   console.log('yes   ' + this.dynamicURL);
